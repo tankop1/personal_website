76a14105a865615ccbb81a7f5cd3dca6ff1dddd9
Tanner <tanner@kopelfamily.com>
2021-05-27 12:52:43 -0500

    Add stats section

--- app.js
+++ app.js
@@ -33,3 +33,26 @@ const titles = document.querySelectorAll('.section-title');
 for (let i = 0; i < sections.length; i++) {
     //setTitleAnimation(sections[i], titles[i]);
 }
+
+// STATS ANIMATIONS
+
+const languageStat = document.getElementById('languages-stat');
+const experienceStat = document.getElementById('experience-stat');
+const optionStat = document.getElementById('option-stat');
+
+function setNextNumber() {
+    if (parseInt(languageStat.innerHTML) < 8) {
+        languageStat.innerHTML = parseInt(languageStat.innerHTML) + 1;
+    }
+
+    if (parseInt(experienceStat.innerHTML) < 16) {
+        experienceStat.innerHTML = parseInt(experienceStat.innerHTML) + 1;
+    }
+}
+
+setInterval(setNextNumber, 100);
+setInterval(() => {
+    if (parseInt(optionStat.innerHTML) < 100) {
+        optionStat.innerHTML = parseInt(optionStat.innerHTML) + 1 + '%';
+    }
+}, 25);
