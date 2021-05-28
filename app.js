395089db37b55d454860f72439be9010e287d988
Tanner <tanner@kopelfamily.com>
2021-05-27 21:32:06 -0500

    Add portfolio-section

--- app.js
+++ app.js
@@ -39,6 +39,7 @@ for (let i = 0; i < sections.length; i++) {
 const languageStat = document.getElementById('languages-stat');
 const experienceStat = document.getElementById('experience-stat');
 const optionStat = document.getElementById('option-stat');
+const statSection = document.getElementById('stats');
 
 function setNextNumber() {
     if (parseInt(languageStat.innerHTML) < 8) {
@@ -50,9 +51,44 @@ function setNextNumber() {
     }
 }
 
-setInterval(setNextNumber, 100);
-setInterval(() => {
-    if (parseInt(optionStat.innerHTML) < 100) {
-        optionStat.innerHTML = parseInt(optionStat.innerHTML) + 1 + '%';
+window.addEventListener('scroll', function () {
+    if (window.scrollY > (statSection.offsetTop - 600)) {
+        setInterval(setNextNumber, 1000);
+        setInterval(() => {
+            if (parseInt(optionStat.innerHTML) < 100) {
+                optionStat.innerHTML = parseInt(optionStat.innerHTML) + 1 + '%';
+            }
+        }, 300);
     }
-}, 25);
+});
+
+// SKILLS GRAPH ANIMATIONS
+
+const skillsSection = document.getElementById('skills');
+const barGraphs = document.querySelectorAll('.bar-graph');
+const widths = ['385px', '535px', '460px', '310px', '500px', '420px'];
+
+window.addEventListener('scroll', function () {
+    if (window.scrollY > (skillsSection.offsetTop - 400)) {
+        for (let i = 0; i < barGraphs.length; i++) {
+            barGraphs[i].style.width = widths[i];
+        }
+    }
+});
+
+// PROJECT HOVER DESCRIPTIONS
+
+const projectContainers = document.querySelectorAll('.project-container');
+const projectDescriptions = document.querySelectorAll('.project-desc');
+const projectDescriptionContainers = document.querySelectorAll('.project-desc-container');
+
+for (let i = 0; i < projectContainers.length; i++) {
+    projectContainers[i].addEventListener('mouseenter', function () {
+        projectDescriptionContainers[i].style.height = '100%';
+        projectDescriptions[i].hidden = false;
+    });
+    projectContainers[i].addEventListener('mouseleave', function () {
+        projectDescriptionContainers[i].style.height = '0%';
+        projectDescriptions[i].hidden = true;
+    });
+}
